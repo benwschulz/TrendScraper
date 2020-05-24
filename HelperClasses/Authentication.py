8992e578466b7b83e5701c815693c8dc2d82d579
Laptop <benwschulz@gmail.com>
2020-05-24 13:47:39 -0500

    Added authentication class

---
+++ HelperClasses/Authentication.py
@@ -0,0 +1,5 @@
+class Authentication:
+    consumer_key = "KhYTSB3pIQQDcUz0IVJIic1CI"
+    consumer_secret = "D1raAZPBZnTTGGpRky6evgCIdIDJv5Hkxtgur7gZmHw0ffcjOT"
+    access_token_secret = "I3plHF0ObiWdnYcL4udQDbl2oWvdlxj9lTJRCl5hS17gY"
+    access_token_key = "1264304925263224832-3rTlvR46Vnym1K1reKAAWmUZbPDOXG"
