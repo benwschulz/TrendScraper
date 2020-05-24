a62b73f7634b314e3fafe1b77d1270442a1f9c6b
Laptop <benwschulz@gmail.com>
2020-05-24 10:59:32 -0500

    Added main script file

---
+++ main.py
@@ -0,0 +1,23 @@
+import tweepy
+
+consumer_key="KhYTSB3pIQQDcUz0IVJIic1CI"
+consumer_secret="D1raAZPBZnTTGGpRky6evgCIdIDJv5Hkxtgur7gZmHw0ffcjOT"
+access_token_secret = "I3plHF0ObiWdnYcL4udQDbl2oWvdlxj9lTJRCl5hS17gY"
+access_token_key = "1264304925263224832-3rTlvR46Vnym1K1reKAAWmUZbPDOXG"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+api = tweepy.API(auth)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
