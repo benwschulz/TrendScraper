7122d9d0e698ce5288395908d885be8322fb3a8f
Laptop <benwschulz@gmail.com>
2020-05-24 13:55:03 -0500

    Started adding init

--- HelperClasses/Authentication.py
+++ HelperClasses/Authentication.py
@@ -3,3 +3,5 @@ class Authentication:
     consumer_secret = "D1raAZPBZnTTGGpRky6evgCIdIDJv5Hkxtgur7gZmHw0ffcjOT"
     access_token_secret = "I3plHF0ObiWdnYcL4udQDbl2oWvdlxj9lTJRCl5hS17gY"
     access_token_key = "1264304925263224832-3rTlvR46Vnym1K1reKAAWmUZbPDOXG"
+
+    def __init__(self):
