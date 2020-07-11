693d978928d44174a646622648d0db84a8cf1aa1
Laptop <benwschulz@gmail.com>
2020-07-11 18:32:38 -0500

    Main now plots tweet locations on a map of the world for each day in the last seven days

--- main.py
+++ main.py
@@ -3,34 +3,40 @@ from HelperClasses.Authentication import Authentication
 from HelperClasses.TweetResults import TweetResults
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+from datetime import date, timedelta
+
+"""Plots the locations of the given result set on a map of the world"""
+def drawMap(results, date):
+    plt.title(date)
+    plt.figure(figsize=(12, 8))
+
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_global()
+    ax.stock_img()
+    ax.coastlines()
+
+    for tweet in results:
+        if tweet.user.baseData.location != "":
+            if tweet.user.coordinates is not None:
+                lat = tweet.user.coordinates[0]
+                long = tweet.user.coordinates[1]
+                plt.plot(long, lat, markersize=4, marker='o', color='red')
+
+    plt.show()
+
 
 api = Authentication().api
 
 mainTag = "covid19"
 resultCount = 20
-tweets = tweepy.Cursor(api.search, q=mainTag, tweet_mode='extended', lang="en").items(resultCount)
 
-fig = plt.figure(figsize=(12, 8))
-ax = plt.axes(projection=ccrs.PlateCarree())
-ax.set_global()
-ax.stock_img()
-ax.coastlines()
-plt.title('#' + mainTag)
+for i in range(1, 8):
+    startDate = date.today() - timedelta(days=i)
 
-resultData = TweetResults(tweets)
+    tweets = tweepy.Cursor(api.search, q=mainTag, until=startDate, tweet_mode='extended', lang="en").items(resultCount)
 
-for tweet in resultData.tweets:
-    if tweet.user.baseData.location != "":
-        if tweet.user.coordinates is not None:
-            lat = tweet.user.coordinates[0]
-            long = tweet.user.coordinates[1]
-            plt.plot(long, lat, markersize=4, marker='o', color='red')
-            print(tweet.user.baseData.location, ":", tweet.text)
-            print("")
-
-plt.show()
-
-
+    resultData = TweetResults(tweets)
+    drawMap(resultData.tweets, startDate)
 
 
 
